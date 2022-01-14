46c1aa91cdf9e8327443ec0f7f0df7b782664bf4
AVespaIsNotAMotorcycle <samuelmebersole@gmail.com>
2022-01-14 12:44:08 -0500

    can now castle

--- board.py
+++ board.py
@@ -7,6 +7,9 @@ class Board:
         self.points = [0,0]
         self.enpassent = 'none'
         self.gamestate = ""
+        # castlereqs[0] = white left rook hasn't moved, king hasn't moved, right rook hasn't moved
+        # castlereqs[1] = black left rook hasn't moved, king hasn't moved, right rook hasn't moved
+        self.castlereqs = [[True,True,True],[True,True,True]]
         for i in range(100):
             # Borders
             if i < 9:
@@ -20,7 +23,7 @@ class Board:
                 else:
                     self.gamestate += str(i)[0]
             elif i % 10 == 9:
-                self.gamestate += '#\n'
+                self.gamestate += '#'
             elif i >= 90:
                 self.gamestate += '#'
 
@@ -36,20 +39,18 @@ class Board:
                         self.gamestate += '-'
                     else:
                         self.gamestate += ' '
-        
+
 
     # render() method
     # outputs board state to console
     def render(self):
         #for piece in self.pieces:
-        #    print(f'{piece.getname()}, {piece.getteam()}')
         print(f'Points: White {self.points[0]}, Black {self.points[1]}')
         rstate = ''
-        for c in self.gamestate:
-            if c != '\n':
-                rstate += ' ' + c + ' '
-            else:
-                rstate += c
+        for index, c in enumerate(self.gamestate):
+            rstate += ' ' + c + ' '
+            if index % 10 == 9:
+                rstate += '\n'
         print(rstate)
 
     # fetchpiece(c)
@@ -67,12 +68,10 @@ class Board:
     # and a scalar d, the destination coords
     # and a piece p, the piece being moved
     def moveisvalid(self, s, d, p):
-        # print(f"Checking: move {p.getname()} from {s} to {d}")
         #Pawn Exceptions
         if p.getname() == "Pawn":
             # 2 tile move
             if abs(d[1] - s[1]) == 2 and s[0] == d[0]:
-            #    print('2 tile move')
                 if d[0] != s[0]:
                     return False
                 if p.getteam() == 1:
@@ -85,31 +84,33 @@ class Board:
                         return True
             # Capturing
             elif self.fetchpiece(d) != 'none' and abs(s[0] - d[0]) == 1 and s[1] - d[1] == -1 * (2 * (p.getteam() % 2) - 1):
-            #    print('valid capture')
                 return True
             # Standard move
             elif s[0] - d[0] == 0 and s[1] - d[1] == -1 * (2 * (p.getteam() % 2) - 1) and self.fetchpiece(d) == 'none':
-            #    print('normal move')
                 return True
             # En passent
             elif d == self.enpassent and abs(s[0] - d[0]) == 1 and s[1] - d[1] == -1 * (2 * (p.getteam() % 2) - 1):
-            #    print('valid enpassent')
                 return True
             else:
-            #    print('invalid move')
+                return False
+
+        # King exceptions
+        if p.getname() == "King" and s[1] == d[1] and ((d[0] == s[0] - 2 and self.castlereqs[p.getteam() - 1][0] and self.castlereqs[p.getteam() - 1][1]) or (d[0] == s[0] + 2 and self.castlereqs[p.getteam() - 1][1] and self.castlereqs[p.getteam() - 1][2])):
+            if d[0] == s[0] + 2 and self.fetchpiece((s[0] + 1,s[1])) == 'none' and self.fetchpiece((s[0] + 2,s[1])) == 'none' and self.fetchpiece((s[0] + 3,s[1])) == 'none':
+                return True
+            elif d[0] == s[0] - 2 and self.fetchpiece((s[0] - 1,s[1])) == 'none' and self.fetchpiece((s[0] - 2,s[1])) == 'none' and self.fetchpiece((s[0] - 3,s[1])) == 'none' and self.fetchpiece((s[0] - 4, s[1])):
+                return True
+            else:
                 return False
 
         if d[0] < 1 or d[0] > 8 or d[1] < 1 or d[1] > 8:
             return False
         moves = p.getmoves()
         if self.fetchpiece(d) != 'none' and self.fetchpiece(d)[1].getteam() == p.getteam():
-            # print('Square occupied by teammate')
             return False
         for move in moves:
-            # print(f'{move} {s} {d}')
             # Check diagonal moves
             if move == ('x','x') and abs(s[0] - d[0]) == abs(s[1] - d[1]):
-                # print("Checking both axes")
                 tracker = s
                 cx = 1
                 cy = 1
@@ -118,7 +119,6 @@ class Board:
                 if d[1] < s[1]:
                     cy = -1
                 while tracker != d:
-                    # print(f"(x,x) check {tracker}")
                     tracker = (tracker[0] + cx, tracker[1] + cy)
                     if tracker == d:
                         return True
@@ -127,13 +127,11 @@ class Board:
                             break
             # Check horizontal moves
             elif move == ('x',0) and (s[1] - d[1]) == 0:
-                # print("Checking x-axis")
                 tracker = s
                 coeff = 1
                 if (s[0] - d[0]) > 0:
                     coeff = -1
                 while tracker != d:
-                    # print(f"(x,0) check {tracker}")
                     tracker = (tracker[0] + (1 * coeff), tracker[1])
                     if tracker == d:
                         return True
@@ -142,13 +140,11 @@ class Board:
                             break
             # Check vertical moves
             elif move == (0,'x') and (s[0] - d[0]) == 0:
-                # print("Checking y-axis")
                 tracker = s
                 coeff = 1
                 if (s[1] - d[1]) > 0:
                     coeff = -1
                 while tracker != d:
-                    # print(f"(0,x) check {tracker}")
                     tracker = (tracker[0], tracker[1] + (1 * coeff))
                     if tracker == d:
                         return True
@@ -165,13 +161,25 @@ class Board:
     # and a scalar d, the destination coords
     # and a piece p, the piece being moved
     def movepiece(self, s, d, p):
-        # print('movepiece()')
+        # Castling
+        rf = -1
+        rn = -1
+        if p.getname() == "King" and abs(s[0] - d[0]) == 2 and s[1] == d[1]:
+            self.castlereqs[p.getteam() - 1] = (False, False, False)
+            p.setcoords(d)
+            if d[0] > s[0]:
+                rf = self.coordstoindex((8,d[1]))
+                self.fetchpiece((8,s[1]))[1].setcoords((d[0]-1,d[1]))
+                rn = self.coordstoindex((d[0]-1,d[1]))
+            else:
+                rf = self.coordstoindex((1,d[1]))
+                self.fetchpiece((1,s[1]))[1].setcoords((d[0]+1,d[1]))
+                rn = self.coordstoindex((d[0]+1,d[1]))
+        # Enpassent
         e = -1
         if d == self.enpassent and p.getname() == "Pawn":
-            # print('enpassent')
             t = (d[0], d[1] + (-1 * ((2 * (p.getteam() % 2)) - 1)))
             e = self.coordstoindex(t)
-            # print(t)
             self.points[p.getteam() - 1] += self.fetchpiece(t)[1].getpoints()
             self.pieces.pop(self.fetchpiece(t)[0])
         elif p.getname() == "Pawn" and abs(s[1] - d[1]) == 2:
@@ -187,6 +195,7 @@ class Board:
         p.setcoords(d)
         i = self.coordstoindex(s)
         k = self.coordstoindex(d)
+        # Pawn Promotion
         if p.getname() == "Pawn" and ((d[1] == 8 and p.getteam() == 1) or (d[1] == 1 and p.getteam() == 2)):
             promo = input("Choose a piece:")
             if p.getteam() == 1:
@@ -219,8 +228,15 @@ class Board:
                     ngs += '-'
                 else:
                     ngs += ' '
+            elif c == rf:
+                if self.indextocoords(rf)[0] % 2 == self.indextocoords(rf)[1] % 2:
+                    ngs += '-'
+                else:
+                    ngs += ' '
             elif c == k:
                 ngs += p.geticon()
+            elif c == rn:
+                ngs += self.fetchpiece(self.indextocoords(rn))[1].geticon()
             else:
                 ngs += self.gamestate[c]
         self.gamestate = ngs
@@ -231,7 +247,6 @@ class Board:
     # to see if it would result in that
     # player being in check
     def resultsincheck(self,s,d,p):
-        # print('resultsincheck()')
         ngs = ""
         i = self.coordstoindex(s)
         k = self.coordstoindex(d)
@@ -254,7 +269,6 @@ class Board:
             if piece.getteam() != p.getteam():
                 if p.getname() != 'King':
                     if self.moveisvalid(piece.getcoords(), tk.getcoords(), piece):
-                        print(f'{piece.getcoords()} {tk.getcoords()}')
                         return True
                 else:
                     if self.moveisvalid(piece.getcoords(), d, piece):
@@ -277,13 +291,11 @@ class Board:
     # playermated(p) method
     # returns whether a player p is in checkmate
     def playermated(self,p):
-        # print('Checking for mate')
         for index, piece in enumerate(self.pieces):
             if piece.getteam() == p:
                 for i in range(99):
                     if self.moveisvalid(piece.getcoords(), self.indextocoords(i),piece):
                         if not self.resultsincheck(piece.getcoords(),self.indextocoords(i),piece):
-                            # print(f'valid move: {piece.getcoords()} {self.indextocoords(i)} {piece.getname()}')
                             return False
         return True
 
@@ -303,4 +315,4 @@ class Board:
     # where x and y are both integers 0 - 9
     # and converts it to an integer 0 - 99
     def coordstoindex(self,c):
-        return c[0] + ( c[1] * 10 ) + c[1]
+        return c[0] + ( c[1] * 10 )
