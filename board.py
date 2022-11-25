02f0e11573f3018163d011687451cdb23c2bfed7
sam <samuelmebersole@gmail.com>
2022-11-25 07:26:20 -0500

    improved code readibility

--- board.py
+++ board.py
@@ -1,5 +1,50 @@
 from piece import Piece
 
+def generatepieces():
+    pieces = []
+
+    # White
+    # Major pieces
+    pieces.append(Piece("Rook",'R',[('x',0),(0,'x')],1,(1,1),5))
+    pieces.append(Piece("Knight",'N',[(-2,-1),(-2,1),(-1,-2),(-1,2),(1,-2),(1,2),(2,-1),(2,1)],1,(2,1),3))
+    pieces.append(Piece("Bishop",'B',[('x','x')],1,(3,1),3))
+    pieces.append(Piece("Queen",'Q',[('x',0),('x','x'),(0,'x')],1,(4,1),9))
+    pieces.append(Piece("King",'K',[(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)],1,(5,1),0))
+    pieces.append(Piece("Bishop",'B',[('x','x')],1,(6,1),3))
+    pieces.append(Piece("Knight",'N',[(-2,-1),(-2,1),(-1,-2),(-1,2),(1,-2),(1,2),(2,-1),(2,1)],1,(7,1),3))
+    pieces.append(Piece("Rook",'R',[('x',0),(0,'x')],1,(8,1),5))
+    # Pawns
+    pieces.append(Piece("Pawn",'P',[(0,1)],1,(1,2),1))
+    pieces.append(Piece("Pawn",'P',[(0,1)],1,(2,2),1))
+    pieces.append(Piece("Pawn",'P',[(0,1)],1,(3,2),1))
+    pieces.append(Piece("Pawn",'P',[(0,1)],1,(4,2),1))
+    pieces.append(Piece("Pawn",'P',[(0,1)],1,(5,2),1))
+    pieces.append(Piece("Pawn",'P',[(0,1)],1,(6,2),1))
+    pieces.append(Piece("Pawn",'P',[(0,1)],1,(7,2),1))
+    pieces.append(Piece("Pawn",'P',[(0,1)],1,(8,2),1))
+
+    # Black
+    # Major Pieces
+    pieces.append(Piece("Rook",'r',[('x',0),(0,'x')],2,(1,8),5))
+    pieces.append(Piece("Knight",'n',[(-2,-1),(-2,1),(-1,-2),(-1,2),(1,-2),(1,2),(2,-1),(2,1)],2,(2,8),3))
+    pieces.append(Piece("Bishop",'b',[('x','x')],2,(3,8),3))
+    pieces.append(Piece("Queen",'q',[('x',0),('x','x'),(0,'x')],2,(4,8),9))
+    pieces.append(Piece("King",'k',[(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)],2,(5,8),0))
+    pieces.append(Piece("Bishop",'b',[('x','x')],2,(6,8),3))
+    pieces.append(Piece("Knight",'n',[(-2,-1),(-2,1),(-1,-2),(-1,2),(1,-2),(1,2),(2,-1),(2,1)],2,(7,8),3))
+    pieces.append(Piece("Rook",'r',[('x',0),(0,'x')],2,(8,8),5))
+    # Pawns
+    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(1,7),1))
+    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(2,7),1))
+    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(3,7),1))
+    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(4,7),1))
+    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(5,7),1))
+    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(6,7),1))
+    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(7,7),1))
+    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(8,7),1))
+
+    return pieces
+
 class Board:
 
     def __init__(self,pieces):
--- pychess.py
+++ pychess.py
@@ -1,97 +1,85 @@
 from piece import Piece
-from board import Board
-
-def generatepieces():
-    pieces = []
-    
-    # White
-    # Major pieces
-    pieces.append(Piece("Rook",'R',[('x',0),(0,'x')],1,(1,1),5))
-    pieces.append(Piece("Knight",'N',[(-2,-1),(-2,1),(-1,-2),(-1,2),(1,-2),(1,2),(2,-1),(2,1)],1,(2,1),3))
-    pieces.append(Piece("Bishop",'B',[('x','x')],1,(3,1),3))
-    pieces.append(Piece("Queen",'Q',[('x',0),('x','x'),(0,'x')],1,(4,1),9))
-    pieces.append(Piece("King",'K',[(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)],1,(5,1),0))
-    pieces.append(Piece("Bishop",'B',[('x','x')],1,(6,1),3))
-    pieces.append(Piece("Knight",'N',[(-2,-1),(-2,1),(-1,-2),(-1,2),(1,-2),(1,2),(2,-1),(2,1)],1,(7,1),3))
-    pieces.append(Piece("Rook",'R',[('x',0),(0,'x')],1,(8,1),5))
-    # Pawns
-    pieces.append(Piece("Pawn",'P',[(0,1)],1,(1,2),1))
-    pieces.append(Piece("Pawn",'P',[(0,1)],1,(2,2),1))
-    pieces.append(Piece("Pawn",'P',[(0,1)],1,(3,2),1))
-    pieces.append(Piece("Pawn",'P',[(0,1)],1,(4,2),1))
-    pieces.append(Piece("Pawn",'P',[(0,1)],1,(5,2),1))
-    pieces.append(Piece("Pawn",'P',[(0,1)],1,(6,2),1))
-    pieces.append(Piece("Pawn",'P',[(0,1)],1,(7,2),1))
-    pieces.append(Piece("Pawn",'P',[(0,1)],1,(8,2),1))
-
-    # Black
-    # Major Pieces
-    pieces.append(Piece("Rook",'r',[('x',0),(0,'x')],2,(1,8),5))
-    pieces.append(Piece("Knight",'n',[(-2,-1),(-2,1),(-1,-2),(-1,2),(1,-2),(1,2),(2,-1),(2,1)],2,(2,8),3))
-    pieces.append(Piece("Bishop",'b',[('x','x')],2,(3,8),3))
-    pieces.append(Piece("Queen",'q',[('x',0),('x','x'),(0,'x')],2,(4,8),9))
-    pieces.append(Piece("King",'k',[(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)],2,(5,8),0))
-    pieces.append(Piece("Bishop",'b',[('x','x')],2,(6,8),3))
-    pieces.append(Piece("Knight",'n',[(-2,-1),(-2,1),(-1,-2),(-1,2),(1,-2),(1,2),(2,-1),(2,1)],2,(7,8),3))
-    pieces.append(Piece("Rook",'r',[('x',0),(0,'x')],2,(8,8),5))
-    # Pawns
-    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(1,7),1))
-    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(2,7),1))
-    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(3,7),1))
-    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(4,7),1))
-    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(5,7),1))
-    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(6,7),1))
-    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(7,7),1))
-    pieces.append(Piece("Pawn",'p',[(0,-1)],2,(8,7),1))
-
-    return pieces
+from board import Board, generatepieces
 
 # converts chessboard notation to coordinates
 def altocoord(i):
-    return (ord(i[0]) - 96,int(i[1]))
+  # check that i is str of len 2
+  if len(i) != 2:
+    return False
+  # check that i[0] is a letter
+  if i[0].isalpha() == False:
+    return False
+  # check that i[1] is a number
+  if i[1].isdecimal() == False:
+    return False
+  return (ord(i[0]) - 96,int(i[1]))
+
+# handles user input given a turn number 'turn'
+# and a boardstate 'board'
+# returns 1 if user input was valid
+# otherwise returns 0
+def handleinput(turn, board):
+  # tell the user whose turn it is
+  if turn % 2 == 1:
+    print('Turn ' + str(turn) + ': White to move')
+  else:
+    print('Turn ' + str(turn) + ': Black to move')
+ 
+  # handle input
+  mv = input()
+  mv = mv.split()
+  # ensure input has at exactly 2 terms
+  if len(mv) != 2:
+    print('Please input 2 coordinates')
+    return 0
+  
+  s = altocoord(mv[0])
+  d = altocoord(mv[1])
+  # ensure that coordinates are properly formatted
+  if s == False or d == False:
+    print('Please input coordinates in the form "d2"')
+    return 0
+
+  movedpiece = board.fetchpiece(s)[1]
+  # ensure that there's a piece at the first coord
+  if movedpiece == 'o':
+    print('No piece at ' + mv[0])
+    return 0
+  
+  # ensure that the piece is of the correct color
+  if movedpiece.getteam() % 2 != turn % 2:
+    print('Piece at ' + mv[0] + ' is of the wrong color')
+    return 0
+
+  # ensure that the move is valid
+  if board.moveisvalid(s, d, movedpiece) == False:
+    print('Move invalid')
+    return 0
+
+  # ensure that the move would not put the player in check
+  if board.resultsincheck(s, d, movedpiece):
+    print('Move invalid - results in check')
+    return 0
+
+  # if it has passed all the tests above, enact move
+  board.movepiece(s, d, movedpiece)
+  if board.playerincheck(turn % 2 == 1):
+    if board.playermated(turn % 2 == 1):
+      print('Checkmate!')
+    else:
+      print('Check!')
+  return 1
 
 def main():
+    # initialize board
     pieces = generatepieces()
     board = Board(pieces)
     board.render()
     turn = 1
 
+    # handle input
     while(True):
-        if turn == 1:
-            print("White to move")
-        else:
-            print("Black to move")
-        mv = input()
-        mv = mv.split()
-        if len(mv) == 2:
-            s = altocoord(mv[0])
-            d = altocoord(mv[1])
-            movedpiece = board.fetchpiece(s)[1]
-            if movedpiece == 'o':
-                print('No piece at ' + mv[0])
-            else:
-                if board.moveisvalid(s,d,movedpiece) and movedpiece.getteam() == turn:
-                    if board.resultsincheck(s,d,movedpiece):
-                        print("Move invalid - results in check")
-                    else:
-                        print("Move valid")
-                        board.movepiece(s,d,movedpiece) 
-                        if turn == 1:
-                            turn = 2
-                            if board.playerincheck(turn):
-                                if board.playermated(turn):
-                                    print('Checkmate!')
-                                    return
-                                else:
-                                    print('Check!')
-                        else:
-                            turn = 1
-                            if board.playerincheck(turn):
-                                print('Check!')
-                else:
-                    print("Move invalid")
-        else:
-            print('Please input 2 coordinates')
+      turn += handleinput(turn, board);
 
 if __name__ == '__main__':
     main()
